28009605b0bc23e9389138fbc59eed374d0acdcf
Md Maruful Islam <marufemran@gmail.com>
2026-05-06 01:35:23 +0600

    Refactor connection handling in connection.php
    Refactored database connection logic and removed commented code.

--- includes/connection.php
+++ includes/connection.php
@@ -1,22 +1,10 @@
 <?php
-
-$server = "mysql"; // <-- put your container/service name here
-$user = "root";
-$password = ""; // change if needed
+$server = ""mysql";
+$username = "root";
+$password = "";
 $database = "hms_db";
-
-$conn = mysqli_connect($server, $username, $password, $database);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-//$server = ""mysql";
-//$username = "root";
-//$password = "";
-//$database = "hms_db";
-//$connection = mysqli_connect("$server","$username","$password");
-//$select_db = mysqli_select_db($connection, $database);
-//if(!$select_db)
-{
-//	echo("connection terminated");
+$connection = mysqli_connect("$server","$username","$password");
+$select_db = mysqli_select_db($connection, $database);
+if(!$select_db)
+{	echo("connection terminated");
 }
