207755325ae2a7e78b591f9775113de7af4c3c77
Md Maruful Islam <marufemran@gmail.com>
2026-05-06 01:33:21 +0600

    Remove unused database selection from connection.php
    Remove unused database selection statement.

--- includes/connection.php
+++ includes/connection.php
@@ -6,7 +6,6 @@ $password = ""; // change if needed
 $database = "hms_db";
 
 $conn = mysqli_connect($server, $username, $password, $database);
-$select_db = mysqli_select_db($connection, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
