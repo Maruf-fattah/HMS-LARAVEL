0bd9c19f391f80bdd274c5272cbc0d43e26690f1
Md Maruful Islam <marufemran@gmail.com>
2026-05-06 01:32:49 +0600

    Fix database connection variables in connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -5,8 +5,19 @@ $user = "root";
 $password = ""; // change if needed
 $database = "hms_db";
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($server, $username, $password, $database);
+$select_db = mysqli_select_db($connection, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+}
+//$server = ""mysql";
+//$username = "root";
+//$password = "";
+//$database = "hms_db";
+//$connection = mysqli_connect("$server","$username","$password");
+//$select_db = mysqli_select_db($connection, $database);
+//if(!$select_db)
+{
+//	echo("connection terminated");
 }
