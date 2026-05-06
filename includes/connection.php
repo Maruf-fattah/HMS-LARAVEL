120c09c49e74df51f1d66e9aed44c7726c1118e1
Md Maruful Islam <marufemran@gmail.com>
2026-05-06 16:36:30 +0600

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,11 +1,13 @@
 <?php
-$host = "mysql"; // or "mysql" if Docker
-$user = "root";
-$password = "";
-$database = "hms_db";
+// Replace 'mysql_service_name' with the Internal Host from Dokploy
+$host = 'mysql'; 
+$user = 'root';
+$pass = '@Mmi202601927019010'; // Ensure this matches your Dokploy DB settings
+$dbname = 'hms_db';
 
-$conn = mysqli_connect("mysql", "root", "password", "hms_db");
+$con = mysqli_connect($host, $user, $pass, $dbname);
 
-if (!$conn) {
+if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
+?>
