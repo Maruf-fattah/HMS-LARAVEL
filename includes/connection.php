9ac74c793219d2773bb11b9df571017521e213e1
Md Maruful Islam <marufemran@gmail.com>
2026-05-09 22:06:35 +0600

    Correct mysqli_connect parameters in connection.php
    Fix the mysqli_connect function call by adding a missing comma.

--- includes/connection.php
+++ includes/connection.php
@@ -3,7 +3,7 @@ $server = "mysql";
 $username = "root";
 $password = "@Mmi202601927019010";
 $database = "hms_db";
-$conn = mysqli_connect("mysql", "root", "@Mmi202601927019010""hms_db");
+$conn = mysqli_connect("mysql", "root", "@Mmi202601927019010","hms_db");
 $select_db = mysqli_select_db($connection, $database);
 if(!$select_db)
 {
