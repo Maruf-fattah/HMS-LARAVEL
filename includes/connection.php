01c47eef5613c8727701298ad30affa68e1f6da7
Md Maruful Islam <marufemran@gmail.com>
2026-05-09 21:49:41 +0600

    Refactor database connection in connection.php
    Updated database connection parameters and error handling.

--- includes/connection.php
+++ includes/connection.php
@@ -1,14 +1,12 @@
 <?php
-// Use the "Internal Host" name you found in Dokploy here:
-$host = 'mysql'; // If Dokploy says "mysql-123", use "mysql-123"
-$user = 'root';
-$pass = '@Mmi202601927019010'; 
-$dbname = 'hms_db';
-
-$con = mysqli_connect($host, $user, $pass, $dbname);
-
-if (!$con) {
-    // This will tell you if the name is still wrong or if the password is wrong
-    die("Connection failed: " . mysqli_connect_error());
+$server = "Mysql";
+$username = "root";
+$password = "";
+$database = "hms_db";
+$connection = mysqli_connect("$server","$username","$password");
+$select_db = mysqli_select_db($connection, $database);
+if(!$select_db)
+{
+	echo("connection terminated");
 }
 ?>
