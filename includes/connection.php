17d0d7cbc945893b6bda1d3b8d410abb6bdb5913
Md Maruful Islam <marufemran@gmail.com>
2026-05-09 21:53:55 +0600

    Fix MySQL server name case in connection.php
    Corrected the case of 'Mysql' to 'mysql' for consistency.

--- includes/connection.php
+++ includes/connection.php
@@ -1,9 +1,9 @@
 <?php
-$server = "Mysql";
+$server = "mysql";
 $username = "root";
 $password = "@Mmi202601927019010";
 $database = "hms_db";
-$conn = mysqli_connect("Mysql", "root", "@Mmi202601927019010");
+$conn = mysqli_connect("mysql", "root", "@Mmi202601927019010");
 $select_db = mysqli_select_db($connection, $database);
 if(!$select_db)
 {
