0007404ca827321dbbd0c6f5ed14936a32cf229d
Md Maruful Islam <marufemran@gmail.com>
2026-05-09 22:08:10 +0600

    Change MySQL connection password
    Updated database connection credentials to use 'rootpassword'.

--- includes/connection.php
+++ includes/connection.php
@@ -3,7 +3,8 @@ $server = "mysql";
 $username = "root";
 $password = "@Mmi202601927019010";
 $database = "hms_db";
-$conn = mysqli_connect("mysql", "root", "@Mmi202601927019010","hms_db");
+$conn = mysqli_connect("mysql", "root", "rootpassword", "hms_db");
+//                                                   ^ Added comma here
 $select_db = mysqli_select_db($connection, $database);
 if(!$select_db)
 {
