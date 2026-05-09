2c8bc10b49d04ded0d5fbab8ee2cdeb5f327892f
Md Maruful Islam <marufemran@gmail.com>
2026-05-09 22:09:14 +0600

    Update database connection credentials

--- includes/connection.php
+++ includes/connection.php
@@ -3,7 +3,7 @@ $server = "mysql";
 $username = "root";
 $password = "@Mmi202601927019010";
 $database = "hms_db";
-$conn = mysqli_connect("mysql", "root", "rootpassword", "hms_db");
+$conn = mysqli_connect("mysql", "root", "@Mmi202601927019010", "hms_db");
 //                                                   ^ Added comma here
 $select_db = mysqli_select_db($connection, $database);
 if(!$select_db)
